43e0ae522d1f2d5e565eaf8635f6be4892dbba94
Helga Rykov Ibsen <92053112+helgarykov@users.noreply.github.com>
2023-09-07 17:33:23 +0200

    Add proj setup.

---
+++ src/Lab-2/Lab_2/Program.cs
@@ -0,0 +1,21 @@
+﻿public abstract class Program
+{
+    public static void Main()
+    {
+        string input = "0 1 2 3 4 5 6 7 8 9 + - * / =";
+        
+        string[] result = Parse(input);
+
+        // Print the result
+        foreach (string keyName in result)
+        {
+            Console.WriteLine(keyName);
+        }
+        
+        static string[] Parse(string? input)
+        {
+            return input!.Split(' ');
+        }
+    }
+}
+
