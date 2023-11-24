4b910fc4c33e7ccb0b3d39eff286f2a02458f0a1
Helga <helgarykovibsen@MacBook-Pro-tilhrende-Helga.local>
2023-11-24 11:14:22 +0100

    Add lab 4

---
+++ src/Lab-4/Lab_3/Program.cs
@@ -0,0 +1,63 @@
+﻿using Lab_3;
+
+
+var inputFilePath = Directory.GetCurrentDirectory();
+inputFilePath = Directory.GetParent(inputFilePath)!.ToString();
+inputFilePath = Directory.GetParent(inputFilePath)!.ToString();
+inputFilePath = Directory.GetParent(inputFilePath)!.ToString();
+inputFilePath += "/Input/Input.txt";
+
+var lines = File.ReadAllLines(inputFilePath);
+
+
+char[,] GetField(string[] lines)
+{
+    char[,] field = new char[4, 4];
+    for (int i = 0; i < 4; i++)
+    {
+        var result = lines[i].Split(' ');
+        for (int j = 0; j < 4; j++)
+        {
+            field[i, j] = result[j][0]; 
+        }
+    }
+
+    return field;
+}
+
+char[] GetCommands(string[] lines)
+{
+    string[] result = lines[4].Split(' '); 
+    char[] commands = new char[result.GetLength(1)];
+    for (int i = 0; i < commands.Length; i++)
+    {
+        if (!(result[i][0] == 'R' || result[i][0] == 'L' || result[i][0] == 'D' || result[i][0] == 'U'))
+        {
+            commands[i] = result[i][0];
+        }
+
+        else throw new Exception("Invalid command literal");
+    }
+    return commands;
+}
+
+GetField(lines);
+GetCommands(lines);
+
+
+
+
+
+
+
+// var outputFilePath = Directory.GetCurrentDirectory();
+// outputFilePath = Directory.GetParent(outputFilePath)!.ToString();
+// outputFilePath = Directory.GetParent(outputFilePath)!.ToString();
+// outputFilePath = Directory.GetParent(outputFilePath)!.ToString();
+// outputFilePath += "/Output/Output.txt";
+
+
+
+
+
+
---
+++ src/Lab-4/Lab_3Test/2048Test.cs
@@ -0,0 +1,246 @@
+using Lab_3;
+namespace Lab_3Test;
+
+
+public class Tests
+{
+    
+    
+    
+    /*[Test]
+    public void TestGetWidthAndHeightReturnsCorrect()
+    {
+        // GIVEN
+        Board newBoard = new Board();
+        
+        string input = 
+            "7 8\n" +
+            "..p....." +
+            "\n.ppp...." +
+            "\n..p....." +
+            "\n........" +
+            "\n...#...." +
+            "\n...#...#" +
+            "\n#..#####";
+        string[] lines = input.Split("\n");
+        
+        // WHEN
+        newBoard.GetWidthAndHeight(lines);
+        
+        // THEN
+        Assert.That(newBoard.Width, Is.EqualTo(8), "width is not 8");
+        Assert.That(newBoard.Height, Is.EqualTo(7), "height is not 7");
+    }
+    
+    [Test]
+    public void GetFigureFromFile_ShouldReturnExpectedCells()
+    {
+        // GIVEN
+        List<Cell> expectedFigure = new List<Cell>()
+        {
+            new Cell(0, 2),
+            new Cell(1, 1),
+            new Cell(1, 2),
+            new Cell(1, 3),
+            new Cell(2, 2)
+        }; 
+        
+        string input = 
+            "7 8\n" +
+            "..p....." +
+            "\n.ppp...." +
+            "\n..p....." +
+            "\n........" +
+            "\n...#...." +
+            "\n...#...#" +
+            "\n#..#####";
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board();
+        
+        // WHEN
+        List<Cell> actualFigure = newBoard.GetFigureAndLandscape(lines,true);
+        
+        // THEN
+       Assert.That(expectedFigure, Is.EqualTo(actualFigure), "figure is not as expected");
+    }
+    
+    [Test]
+    public void GetLandscapeFromFile_ShouldReturnExpectedCells()
+    {
+        // GIVEN
+        List<Cell> expectedLandscape = new List<Cell>()
+        {
+            new Cell(4, 3),
+            new Cell(5, 3),
+            new Cell(5, 7),
+            new Cell(6, 0),
+            new Cell(6, 3),
+            new Cell(6, 4),
+            new Cell(6, 5),
+            new Cell(6, 6),
+            new Cell(6, 7)
+        }; 
+        
+        string input = 
+            "7 8\n" +
+            "..p....." +
+            "\n.ppp...." +
+            "\n..p....." +
+            "\n........" +
+            "\n...#...." +
+            "\n...#...#" +
+            "\n#..#####";
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board();
+        
+        // WHEN
+        List<Cell> actualLandscape = newBoard.GetFigureAndLandscape(lines,false);
+        
+        // THEN
+        Assert.That(expectedLandscape, Is.EqualTo(actualLandscape), "landscape is not as expected");
+    }
+    [Test]
+    public void ProcessFigureOneCycleTestOne()
+    {
+        // GIVEN
+        const string input = "7 8\n" +
+                             "..p....." +
+                             "\n.ppp...." +
+                             "\n..p....." +
+                             "\n........" +
+                             "\n...#...." +
+                             "\n...#...#" +
+                             "\n#..#####";
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board(lines);
+        
+        List<Cell> originalFigure = newBoard.GetFigureAndLandscape(lines,true);
+        List<Cell> expectedFigure = originalFigure;
+        foreach (var cell in expectedFigure)
+        {
+            cell.X++;
+        }
+        
+        // WHEN
+        newBoard.ProcessFigureOneCycle();
+        
+        
+        // THEN
+        Assert.That(expectedFigure, Is.EqualTo(newBoard.Figure), "figure is not as expected");
+    }
+    
+    [Test]
+    public void ProcessFigureOneCycleBlockedTestOne()
+    {
+        // GIVEN
+        const string input = "7 8\n" +
+                             "..p....." +
+                             "\n.ppp...." +
+                             "\n..p#...." +
+                             "\n...#...." +
+                             "\n...#...." +
+                             "\n...#...#" +
+                             "\n#..#####";
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board(lines);
+        
+        List<Cell> expectedFigure = newBoard.GetFigureAndLandscape(lines,true);
+        
+        
+        // WHEN
+        newBoard.ProcessFigureOneCycle();
+        
+        
+        // THEN
+        Assert.That(expectedFigure, Is.EqualTo(newBoard.Figure), "figure is not as expected");
+    }
+
+    [Test]
+    public void ProcessFigureOneCycleTestTwo()
+    {
+        // GIVEN
+        const string input = "5 6\n" +
+                             "..p..." +
+                             "\n##p.##" +
+                             "\n##pp##" +
+                             "\n##..##" +
+                             "\n##..##";
+
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board(lines);
+
+        List<Cell> originalFigure = newBoard.GetFigureAndLandscape(lines, true);
+        List<Cell> expectedFigure = originalFigure;
+        foreach (var cell in expectedFigure)
+        {
+            cell.X++;
+        }
+
+        // WHEN
+        newBoard.ProcessFigureOneCycle();
+
+        //THEN
+        Assert.That(expectedFigure, Is.EqualTo(newBoard.Figure), "figure is not as expected");
+
+        //TODO: add another test of ProcessFigureOneCycleTest ALSO delegates and stringBuilder.
+    }
+    
+    [Test]
+    public void ProcessFigureSeveralCyclesBlockedTest()
+    {
+        // GIVEN
+        const string input = "5 6\n" +
+                             "..p..." +
+                             "\n##p.##" +
+                             "\n##pp##" +
+                             "\n###.##" +
+                             "\n##..##";
+
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board(lines);
+
+        List<Cell> expectedFigure = newBoard.GetFigureAndLandscape(lines,true);
+
+        // WHEN
+        newBoard.ProcessFigureSeveralCycles();
+
+        //THEN
+        Assert.That(expectedFigure, Is.EqualTo(newBoard.Figure), "figure is not as expected");
+
+        //TODO: add another test of ProcessFigureOneCycleTest ALSO delegates and stringBuilder.
+    }
+    
+    [Test]
+    public void ProcessFigureSeveralCyclesTest()
+    {
+        // GIVEN
+        const string input = "7 8\n" +
+                             "..p....." +
+                             "\n.ppp...." +
+                             "\n..p....." +
+                             "\n........" +
+                             "\n........" +
+                             "\n...#...#" +
+                             "\n#..#####";
+        string[] lines = input.Split("\n");
+        Board newBoard = new Board(lines);
+        
+        List<Cell> originalFigure = newBoard.GetFigureAndLandscape(lines,true);
+        List<Cell> expectedFigure = originalFigure;
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (var cell in expectedFigure)
+            {
+                cell.X++;
+            }
+            
+        }
+        
+        // WHEN
+        newBoard.ProcessFigureSeveralCycles();
+        
+        
+        // THEN
+        Assert.That(expectedFigure, Is.EqualTo(newBoard.Figure), "figure is not as expected");
+    }*/
+}
